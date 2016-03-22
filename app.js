66b60d7b402eebcdadf7a6c8653b9bfdc36ba79f
Mansi <mpatel.y@gmail.com>
2016-03-22 15:24:20 -0400

    doe connect form

--- app.js
+++ app.js
@@ -24,17 +24,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 // Initialize Session
 app.use(session({
@@ -46,6 +35,19 @@ app.use(session({
 // Initialize Flash Messages
 app.use(flash());
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 
 // error handlers
--- routes/index.js
+++ routes/index.js
@@ -23,9 +23,35 @@ router.get('/projects', function(req, res, next) {
   res.render('projects');
 });
 
-//About get request
-router.get('/contact_me', function(req, res, next) {
-  res.render('contact_me');
+/* GET contact page. */
+router.get('/contact_me', function (req, res, next) {
+    req.flash('successmessage', 'Thank You. Your message has been sent.');
+    req.flash('errormessage', 'An Error has occurred.');
+    res.render('contact_me', { title: 'Contact', messages: null });
+});
+/* Email processing */
+router.post('/contact_me', function (req, res, next) {
+    sendgrid.send({
+        to: 'mpatel.y@gmail.com',
+        from: req.body.email,
+        subject: 'Contact Form Submission',
+        text: "This message has been sent from the contact form at [your portfolio]\r\n\r\n" +
+            "Name: " + req.body.name + "\r\n\r\n" +
+            "Phone: " + req.body.phone + "\r\n\r\n" +
+            req.body.message,
+        html: "This message has been sent from the contact form at [your portfolio]<br><br>" +
+            "<strong>Name:</strong> " + req.body.name + "<br><br>" +
+            "<strong>Phone:</strong> " + req.body.phone + "<br><br>" +
+            req.body.message
+    }, function (err, json) {
+        if (err) {
+            res.status(500).json('error');
+        }
+        res.render('contact_me', {
+          //  title: 'Contact',
+            messages: req.flash('successmessage')
+        });
+    });
 });
 
 
